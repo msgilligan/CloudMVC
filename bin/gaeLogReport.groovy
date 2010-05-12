1c1053cb8ef971b649d823e4bd2b988a9e046437
Sean Gilligan <sean@msgilligan.com>
2010-05-11 20:56:25 -0700

    detab

--- bin/gaeLogReport.groovy
+++ bin/gaeLogReport.groovy
@@ -2,36 +2,36 @@
 File logFile = new File(args[0]);
 
 char sepChar = 0
-java.lang.String sepStr = sepChar	 // Character.toString(sepChar);
+java.lang.String sepStr = sepChar    // Character.toString(sepChar);
 writer = new StringWriter()
 builder = new groovy.xml.MarkupBuilder(writer)
 
 html = builder.html {
-	head()
-	body {
-		logFile.eachLine{ line ->
-			parts = line.split(sepStr)
-			if (parts[0])
-			{
-				div(class: "entry") {
-					h4(class: "apache", parts[0]) 
-					if (parts.length > 1)
-					{
-						ul
-						{
-							for (def i = 1; i < parts.length; i++ )
-							{
-								if (parts[i] != ": ")
-								{
-									li(class: "trace", parts[i])
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    head()
+    body {
+        logFile.eachLine{ line ->
+            parts = line.split(sepStr)
+            if (parts[0])
+            {
+                div(class: "entry") {
+                    h4(class: "apache", parts[0]) 
+                    if (parts.length > 1)
+                    {
+                        ul
+                        {
+                            for (def i = 1; i < parts.length; i++ )
+                            {
+                                if (parts[i] != ": ")
+                                {
+                                    li(class: "trace", parts[i])
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 println writer.toString()
