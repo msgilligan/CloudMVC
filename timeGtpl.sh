6e00e8559e7dd10b5661bdb497aa534d03a57ade
Sean Gilligan <sean@msgilligan.com>
2010-06-02 00:05:00 -0700

    change url to login.gtpl to get a standalone .gtpl file

--- timeGtpl.sh
+++ timeGtpl.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -w "\ntime_total = %{time_total}\n" http://cloudmvc.appspot.com -o /dev/null 
+curl -w "\ntime_total = %{time_total}\n" http://cloudmvc.appspot.com/login.gtpl -o /dev/null 
